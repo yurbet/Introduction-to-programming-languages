7dbcd75d8d872825e5447eb4f64c3b75e0b61880
berkut <berkut_cool@mail.ru>
2022-08-22 10:07:29 +0400

    fifth homework

---
+++ Introduction to programming languages/Seminars/HomeWorks/hw5/Program.cs
@@ -0,0 +1,138 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+
+    return count;
+}
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+int even = CountEvenNumbers(myArray);
+Console.WriteLine($"Number of even characters in the array = {even}");
+*/
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int SumUnevenArrayElements(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum += array[i];
+    }
+
+    return sum;
+}
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+int sumUneven = SumUnevenArrayElements(myArray);
+Console.WriteLine($"Sum of elements in an array at odd positions = {sumUneven}");
+*/
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+/*
+double[] CreateRandomArray(int size)
+{
+double [] newArray = new double[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().NextDouble();
+    return newArray;
+}
+*/
+
+double[] CreateArray(int size)
+{
+double [] newArray = new double[size];
+for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input the {i + 1} real element of the array: ");
+        newArray[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return newArray;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double DifferenceMinAndMaxArrayElements(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+        if (array[i] > max)
+            max = array[i];
+    }
+
+    return max - min;
+}
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+// double[] myArray = CreateRandomArray(a);
+double[] myArray = CreateArray(a);
+ShowArray(myArray);
+double difference = DifferenceMinAndMaxArrayElements(myArray);
+Console.WriteLine($"Difference between maximum and minimum array elements = {difference}");
