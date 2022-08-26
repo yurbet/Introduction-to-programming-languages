2cb8d31bb0264c1fe7ab9615f07179a96d65901d
berkut <berkut_cool@mail.ru>
2022-08-26 22:19:28 +0400

    all

---
+++ Introduction to programming languages/Seminars/S2/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача №1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+/*int FindBiggestDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int max;
+
+    if (ed > dec)
+    {
+        max = ed;
+    }
+    else
+    {
+        max =  dec;
+    }
+    return max;
+}
+
+Console.WriteLine("Некотороу двухзначное рандомное число");
+int randomNumber = new Random().Next(10,100);
+
+int biggesstDigit = FindBiggestDigit(randomNumber);
+Console.WriteLine($"Наибольшее число из {randomNumber} это {biggesstDigit}");
+*/
+
+// Задача №2. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+/*int CutNumber(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+
+    return sot * 10 + ed;
+}
+
+int randomNumber = new Random().Next(100,1000);
+
+int biggesstDigit = FindBiggestDigit(randomNumber);
+Console.WriteLine($"Наибольшее число из {randomNumber} это {biggesstDigit}");
+*/
+
+// Задача №3. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+/*void IsDivisionable(int num1, int num2)
+{
+    int reminder = num2 % num1;
+    
+    if (reminder == 0)
+    {
+        Console.WriteLine($"Второе число {num2} кратно первому {num1}");
+    }
+    else
+    {
+        Console.WriteLine($"Второе число {num2} не кратно первому {num1}. Остатко от деления равен {reminder}");
+    }
+}
+
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+IsDivisionable(n1, n2);*/
+
+// Задача №4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+bool IsDivis(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+    /*
+    if (number % 7 && number % 23 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }*/
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+bool isDivison = IsDivis(n);
+Console.WriteLine(isDivison);
---
+++ Introduction to programming languages/Seminars/S3/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача №1. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+/*
+int FindQuart(double x, double y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    if (x < 0 && y > 0)
+        return 2;
+    if (x < 0 && y < 0)
+        return 3;
+    if (x > 0 && y < 0)
+        return 4;
+    
+    return 0;
+}
+
+Console.Write("Введите первое число: ");
+double xA = Convert.ToInt32(Console.Readline());
+Console.Write("Введите первое число: ");
+double xB = Convert.ToInt32(Console.Readline());
+
+int quatNum = FindQuart(xA, xB);
+
+if (quatNum == 0)
+    Console.WriteLine("Точка на осях");
+else
+    Console.WriteLine($"Point is located on {quatNum} quart");
+*/
+
+// Задача №2. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+/*
+void FindCord(int number)
+{
+    if (number == 1)
+        Console.WriteLine("Диапозон (X+ ; Y+)");
+    if (number == 2)
+        Console.WriteLine("Диапозон (X- ; Y+)");
+    if (number == 3)
+        Console.WriteLine("Диапозон (X- ; Y-)");
+    if (number == 4)
+        Console.WriteLine("Диапозон (X+ ; Y-)");
+}
+
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+FindCord(quarter);
+*/
+
+// Задача №3. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+/*
+double FindDistance(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+
+Console.Write("Введите координаты первой точки по оси X : ");
+double Xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты первой точки по оси Y : ");
+double Ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты второй точки по оси X : ");
+double Xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты второй точки по оси Y : ");
+double Yb = Convert.ToDouble(Console.ReadLine());
+
+double distance = FindDistance(Xa, Xb, Ya, Yb);
+Console.WriteLine($"Расстояние между двух точек равно {distance}");
+*/
+// Задача №4. Напишите программу, которая принимает на вход число (N) и выдаёт на консоль квадраты чисел от 1 до N.
+
+void ShowSquare (double number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        double current = Math.Pow(Convert.ToDouble(i), 2);
+        Console.WriteLine($"{current} ");
+    }
+}
+
+Console.Write("Введите любое число: ");
+double digit = Convert.ToInt32(Console.ReadLine());
+ShowSquare(digit);
